d58fc55b19a68ef41eb6ad8811987b23b968562f
Kirill <kirrilf@mail.ru>
2019-03-10 19:51:20 +0300

    all function add and delete work

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -36,7 +36,7 @@ void AddElemenToList(int num)
 	
 }
 
-void AddElementToListByNumber(int num, int numberOfElemet) {
+void AddElementToListByNumber(int num, int numberOfElement) {
 	SingleLinkedList *Element = new SingleLinkedList;
 	SingleLinkedList *temp = new SingleLinkedList;
 	SingleLinkedList *temp1 = new SingleLinkedList; 
@@ -45,12 +45,12 @@ void AddElementToListByNumber(int num, int numberOfElemet) {
 	Element->number = num;
 	temp = firstElement;
 
-	for (int i = 1; i <= numberOfElemet; i++) {
+	for (int i = 1; i <= numberOfElement; i++) {
 		
-		if (i == numberOfElemet-1) {
+		if (i == numberOfElement-1) {
 			temp1 = temp;
 		}
-		else if (i == numberOfElemet) {
+		else if (i == numberOfElement) {
 			temp2 = temp;
 		}
 
@@ -70,6 +70,58 @@ void AddFirstElement(int num) {
 
 }
 
+void DeleteElementToListByNumber(int numberOfElement) {
+
+	SingleLinkedList *temp = new SingleLinkedList;
+	SingleLinkedList *temp1 = new SingleLinkedList;
+	SingleLinkedList *temp2 = new SingleLinkedList;
+	SingleLinkedList *temp3 = new SingleLinkedList;
+
+
+	temp = firstElement;
+
+	for (int i = 1; i <= numberOfElement + 1; i++) {
+
+		if (i == numberOfElement - 1) {
+			temp1 = temp;
+		}
+		else if (i == numberOfElement) {
+			temp2 = temp;
+		}
+		else if (i == numberOfElement + 1) {
+			temp3 = temp;
+			break;
+		}
+
+		temp = temp->next;
+	}
+
+	temp1->next = temp3;
+	delete temp2;
+
+}
+
+void DeleteFirstElement() {
+	SingleLinkedList *temp1 = new SingleLinkedList;
+	SingleLinkedList *temp2 = new SingleLinkedList;
+
+	temp1 = firstElement;
+	temp2 = temp1->next;
+	delete temp1;
+	firstElement = temp2;
+}
+
+void DeleteAllSingleLinkedList() {
+	SingleLinkedList *temp1 = new SingleLinkedList;
+	SingleLinkedList *temp2 = new SingleLinkedList;
+	temp1 = firstElement;
+	while (temp1) {
+		temp2 = temp1;
+		temp1 = temp1->next;
+		delete temp2;
+	}
+}
+
 void ShowSingleLinkedList() {
 	SingleLinkedList *temp = new SingleLinkedList;
 	temp = firstElement;
@@ -79,7 +131,7 @@ void ShowSingleLinkedList() {
 	}
 }
 
-void DeleteElementToListByN
+
 
 
 int main()
@@ -97,6 +149,12 @@ int main()
 	AddFirstElement(1);
 	AddElemenToList(16);
 	AddElementToListByNumber(18, 2);
+	DeleteElementToListByNumber(10);
+	DeleteElementToListByNumber(9);
+	DeleteFirstElement();
+	DeleteElementToListByNumber(7);
+	DeleteAllSingleLinkedList();
+	AddElemenToList(3);
 	ShowSingleLinkedList();
 
 	
